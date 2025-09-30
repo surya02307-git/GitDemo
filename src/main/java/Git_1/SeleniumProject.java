2b6f9624a66f8f3ce902d34090659c696c72aee5
surya <surya02307@gmail.com>
2025-09-30 07:54:34 +0530

    this is commit Assian Time Zone

--- src/main/java/Git_1/SeleniumProject.java
+++ src/main/java/Git_1/SeleniumProject.java
@@ -8,5 +8,13 @@ public class SeleniumProject {
 	public void SeleniumCode() {
 		System.out.println("This is selnium code");
 	}
+	@Test
+	public void SmokeCode() {
+		System.out.println("This is smoke code");
+	}
+	@Test
+	public void SanityCode() {
+		System.out.println("This is sanity code");
+	}
 
 }
