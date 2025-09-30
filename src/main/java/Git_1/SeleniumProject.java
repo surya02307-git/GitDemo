40f9e233323df662459adf534fb6dc80d5a5c6c7
surya <surya02307@gmail.com>
2025-09-30 07:11:59 +0530

    My First Commit

---
+++ src/main/java/Git_1/SeleniumProject.java
@@ -0,0 +1,12 @@
+package Git_1;
+
+import org.testng.annotations.Test;
+
+public class SeleniumProject {
+	
+	@Test
+	public void SeleniumCode() {
+		System.out.println("This is selnium code");
+	}
+
+}
